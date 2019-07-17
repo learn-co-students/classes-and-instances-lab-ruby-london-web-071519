0f55d533422e650a0e9654ccd692a4a1e1b68651
Harry Chopra <harrychopra@gmail.com>
2019-07-17 08:55:08 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+# Defining class Dog
+
+class Dog
+
+end
+
+#Create three dogs
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+#Define person class
+
+class Person
+    
+end
+
+#Create two people
+
+adele_goldberg = Person.new
+alan_kay = Person.new
